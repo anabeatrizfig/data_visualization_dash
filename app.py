f492c14f46b55f91fe540f2012bc484222128fff
Ana Beatriz Oliveira <anabeatrizfig2@gmail.com>
2022-04-13 13:57:33 +0100

    add css and filters

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ pd.set_option('display.float_format', lambda x: '%.6f' % x)
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 
 # Data Preprocessing
@@ -16,93 +17,94 @@ eu_data['NonRenewable energy consumption (%) 2015'] = eu_data['NonRenewable ener
 eu_data['Renewable energy consumption (%) 2015'] = eu_data['Renewable energy consumption (TJ) 2015']/eu_data['Total final energy consumption (TFEC) (TJ) 2015']
 
 
-# Building the Plots
+country_options = [dict(label=country, value=country) for country in eu_data['Countries'].unique()]
 
-# 1-MAP
 # token to use a free type of map from mapbox
+
 mapbox_token = 'pk.eyJ1IjoiYW5hYmVhdHJpemZpZyIsImEiOiJjbDF0NXZ4dW4yNjFrM2pxcnp1ZzQ1cGl1In0.vpjAAtkv4flG_jtgOHcDQw'
+# 1-MAP - fixed - no filter applied
 
 data_scattermap = dict(type='scattermapbox',
-                       lat=eu_data['lat'], 
-                       lon=eu_data['lon'],
-                       mode=['markers', 'lines', 'text'][0],
-                       #hover_name=eu_data['Countries'], hover_data=eu_data['Population, total 2022'],
-                       #hoverinfo='name',
-                       text=eu_data['Countries'],
-                       customdata=round(eu_data['Population, total 2022']/1000000,1),
-                       marker=dict(color='blue',
-                                   opacity=0.7,
-                                   size=eu_data['Population, total 2022']/2000000
-                                   
-                                  ),
-                       hovertemplate="<b>Country: </b> %{text} <br>" +
-                                     "<b>Population (2022): </b> %{customdata}M <br>",
-                                     #"<b>GDP per capita (2020): </b> %{customdata[1]}" ,
-                       hovertext='name'
-                      )
+                    lat=eu_data['lat'], 
+                    lon=eu_data['lon'],
+                    mode=['markers', 'lines', 'text'][0],
+                    #hover_name=eu_data['Countries'], hover_data=eu_data['Population, total 2022'],
+                    #hoverinfo='name',
+                    text=eu_data['Countries'],
+                    customdata=round(eu_data['Population, total 2022']/1000000,1),
+                    marker=dict(color='blue',
+                                opacity=0.7,
+                                size=eu_data['Population, total 2022']/2000000
+                                
+                                ),
+                    hovertemplate="<b>Country: </b> %{text} <br>" +
+                                    "<b>Population (2022): </b> %{customdata}M <br>",
+                                    #"<b>GDP per capita (2020): </b> %{customdata[1]}" ,
+                    hovertext='name'
+                    )
 
 
 layout_scattermap = dict(mapbox=dict(style='light',
-                                     accesstoken=mapbox_token,
-                                     #layers=[dict(source=feature) for feature in data_geo['features']],
-                                     center=dict(lat=eu_data['lat'].iloc[5],
-                                                 lon=eu_data['lon'].iloc[5]
+                                    accesstoken=mapbox_token,
+                                    #layers=[dict(source=feature) for feature in data_geo['features']],
+                                    center=dict(lat=eu_data['lat'].iloc[5],
+                                                lon=eu_data['lon'].iloc[5]
                                                 ),
-                                     zoom=2.5
+                                    zoom=2.5
                                     ),
-                         autosize=True,
-                         margin=dict(
+                        autosize=True,
+                        margin=dict(
                                     l=30,
                                     r=30,
                                     b=20,
                                     t=40
                                     ),
-                         title=dict(text='World Map',
+                        title=dict(text='World Map',
                                     x=.5
-                                   ),
-                         hovermode='closest'
-                         
+                                ),
+                        hovermode='closest'
+                        
                         )
 
 fig_scattermap = go.Figure(data=data_scattermap, layout=layout_scattermap)
 
+# Building the Plots
 # 2-BARCHART
 
 data = eu_data[['Countries',
-                'Renewable energy consumption (%) 2015',
-                'NonRenewable energy consumption (%) 2015',
-                'Total final energy consumption (TFEC) (TJ) 2015'
-               ]].sort_values(by='Renewable energy consumption (%) 2015')
+        'Renewable energy consumption (%) 2015',
+        'NonRenewable energy consumption (%) 2015',
+        'Total final energy consumption (TFEC) (TJ) 2015'
+    ]].sort_values(by='Renewable energy consumption (%) 2015')
 
 data_bar1 = dict(type='bar',
-                orientation='h',
-                x=round(data['Renewable energy consumption (%) 2015'],2),
-                y=data['Countries'],
-                hovertemplate="<b>Country: </b> %{y} <br>" +
-                              "<b>Energy (TJ): </b> %{x} <br>",
-                name='Renewable',
-                marker_color='green'
-               )
+            orientation='h',
+            x=round(data['Renewable energy consumption (%) 2015'],2),
+            y=data['Countries'],
+            hovertemplate="<b>Country: </b> %{y} <br>" +
+                        "<b>Energy (TJ): </b> %{x} <br>",
+            name='Renewable',
+            marker_color='green'
+        )
 
 data_bar2 = dict(type='bar',
-                orientation='h',
-                x=round(data['NonRenewable energy consumption (%) 2015'],2),
-                y=data['Countries'],
-                hovertemplate="<b>Country: </b> %{y} <br>" +
-                              "<b>Energy (TJ): </b> %{x} <br>",
-                name='NonRenewable',
-                marker_color='gray'
-               )
+            orientation='h',
+            x=round(data['NonRenewable energy consumption (%) 2015'],2),
+            y=data['Countries'],
+            hovertemplate="<b>Country: </b> %{y} <br>" +
+                        "<b>Energy (TJ): </b> %{x} <br>",
+            name='NonRenewable',
+            marker_color='gray'
+        )
 
 layout_bar = dict(title=dict(
                         text='Renewable and NonRenewable energy consumption (2015)',
                         x=.5
                         ),
-                  xaxis=dict(title='Energy Consumption (%)'),
-                  yaxis=dict(title='Country'),
-                  barmode='stack'
-                 )
-
+                xaxis=dict(title='Energy Consumption (%)'),
+                yaxis=dict(title='Country'),
+                barmode='stack'
+                )
 fig_bar = go.Figure(data=[data_bar1,data_bar2] , layout=layout_bar)
 
 # 3-SCATTERPLOT
@@ -149,80 +151,93 @@ app = dash.Dash(__name__)
 
 server = app.server
 
-app.layout = html.Div(children=[
-    # All elements from the top of the page
+app.layout = html.Div([
     html.Div([
-        html.H1(children='Hello Dash'),
-
-        html.Div(children='''
-            Dash: A web application framework for Python.
-        '''),
-
-        dcc.Graph(
-            id='map',
-            figure=fig_scattermap
-        ),  
-    ]),
-    # New Div for all elements in the new 'row' of the page
+        html.H1('Energy - Renewable')
+    ], className='Title'),
     html.Div([
-        html.H1(children='Hello Dash', style = {'textAlign':'center','marginTop':20,'marginBottom':20, 'backgroundColor': 'gray'}),
-
-        html.Div(children='''
-            Dash: A web application framework for Python.
-        '''),
-
-        dcc.Graph(
-            id='bar',
-            figure=fig_bar
-        ),  
-    ]),
-    # New Div for all elements in the new 'row' of the page
+        html.Div([
+            dcc.Slider(
+                id='year_slider',
+                min=2000,
+                max=2020,
+                marks={str(i): '{}'.format(str(i)) for i in
+                        [2000, 2005, 2010, 2020]},
+                value=2000,
+                step=1)],
+            className='column_two'
+            ),
+        html.Div([
+            dcc.Dropdown(
+                id='country_drop',
+                options=country_options,
+                value=['Portugal'],
+                multi=True
+            )],
+            className='column_two'
+            )
+        ],
+        className='row'),
     html.Div([
-        html.H1(children='Hello Dash'),
-
-        html.Div(children='''
-            Dash: A web application framework for Python.
-        '''),
-
-        dcc.Graph(
-            id='scatter',
-            figure=fig_scatter,
-            style={'display': 'inline-block'}
-        ),
-        dcc.Graph(
-            id='bar2',
-            figure=fig_bar,
-            style={'display': 'inline-block'}
-        ),
-
-    ]),
-])
-
-
-'''app.layout = html.Div(
-    html.Div(
-        children=[
-            html.H1(children='My First DashBoard'),
-            html.Div(children='Example of html Container'),
+        html.Div([
+            html.H2(children='DASH title XXXXX'),
             dcc.Graph(
-                id='map-graph',
+                id='map',
                 figure=fig_scattermap
                 )
-                ]
-            ),
-    html.Div(
-        children=[
-        html.H1(children='second graph'),
-        html.Div(children='Example of html Container'),
-        dcc.Graph(
-            id='bar-graph',
-            figure=fig_bar
+            ],
+            className='column_two'),
+        html.Div([
+            html.H2(children='DASH title XXXXX'),
+            dcc.Graph(
+                id='scatter2',
+                figure=fig_scatter
+                # ,
+                # style={'display': 'inline-block', 'with': '30%'}
+                )
+                ],
+                className='column1'
             )
-        ]
-    )
-)
-'''
+        ],
+        className='row'),
+    html.Div([
+        html.Div([
+            html.H2(children='DASH title XXXXX'),
+            dcc.Graph(
+                id='scatter',
+                figure=fig_scatter,
+                style={'display': 'inline-block', 'with': '90%'}
+                )
+                ],
+                className='column_two'),
+        html.Div([
+            html.H2(children='DASH title XXXXX'),
+            dcc.Graph(
+                id='bar2',
+                figure=fig_bar,
+                style={'display': 'inline-block', 'with': '90%'}
+                )
+        ],
+        className='column_two')
+    ],
+    className='row')
+     
+])
 
+# @app.callback(
+#     [
+#         Output('fig_bar', 'figure'),
+#         Output('fig_scattermap', 'figure'),
+#         Output('fig_scatter', 'figure'),
+#     ],
+#     [
+#         Input('button', 'n_clicks')
+#     ],
+#     [
+#         # State('year_slider', 'value'),
+#         State('country_drop', 'value')
+#     ]
+# )
 
 
 if __name__ == '__main__':
